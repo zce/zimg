42f48e99e9e86d949b6a5c9cba5d6e778af0bdd5
zce <w@zce.me>
2023-11-18 10:58:40 +0800

    feat: filename option & skip optimized

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 import cac from 'cac'
 
-import { name, version } from '../package.json'
 import { zimg } from '.'
+import { name, version } from '../package.json'
 
 const cli = cac(name).version(version).help()
 
 cli
   .command('[cwd]', 'Optimize images for production via sharp')
   .option('-p, --pattern <pattern>', 'Glob pattern to match files, default to `**/*.{jpg,jpeg,png,gif,webp}`')
+  .option('-n, --filename <filename>', 'Filename of generated images, default to `[name].[size].webp`')
   .option('-s, --sizes <sizes>', 'Sizes to generate, default to `[640, 720, 1280, 1600]`')
   .option('-q, --quality <quality>', 'Quality of generated images, default to `75`')
-  .action((cwd, { pattern, sizes, quality }) => {
-    return zimg({ cwd: cwd ?? 'public', pattern, sizes, quality })
+  .option('-f, --force', 'Force to optimize images, default to `false`')
+  .action((cwd, { pattern, filename, sizes, quality, force }) => {
+    return zimg({ cwd: cwd ?? 'public', pattern, filename, sizes, quality, skipOptimized: !force })
   })
 
 const onError = (err: Error): void => {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { access } from 'fs/promises'
 import glob from 'fast-glob'
 import ora from 'ora'
 import sharp from 'sharp'
@@ -12,6 +13,10 @@ export interface ZImgOptions {
    */
   pattern?: string
   /**
+   * Filename of generated images, default to `[name].[size].webp`
+   */
+  filename?: string
+  /**
    * Sizes to generate, default to `[640, 720, 1280, 1600]`
    */
   sizes?: number[]
@@ -19,15 +24,35 @@ export interface ZImgOptions {
    * Quality of generated images, default to `75`
    */
   quality?: number
+  /**
+   * Skip optimized images, default to `true`
+   */
+  skipOptimized?: boolean
 }
 
 export const zimg = async (options: ZImgOptions = {}) => {
   const spinner = ora('optimizing images').start()
 
-  const pattern = options.pattern ?? '**/*.{jpg,jpeg,png,gif}'
   const cwd = options.cwd ?? process.cwd()
+  const pattern = options.pattern ?? '**/*.{jpg,jpeg,png,gif}'
+  const filename = options.filename ?? '[name].[size].webp'
   const sizes = options.sizes ?? [640, 720, 1280, 1600]
   const quality = options.quality ?? 75
+  const skipOptimized = options.skipOptimized ?? true
+
+  const exists = async (path: string) => {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  const getFilename = (file: string, size: number) => {
+    const name = file.replace(/\.[a-z]+$/i, '')
+    return filename.replace(/\[name\]/g, name).replace(/\[size\]/g, size.toString())
+  }
 
   const files = await glob(pattern, { cwd, absolute: true })
 
@@ -37,8 +62,10 @@ export const zimg = async (options: ZImgOptions = {}) => {
     if (width == null || height == null) return
     const aspectRatio = width / height
     await Promise.all(
-      sizes.map(size =>
-        image
+      sizes.map(async size => {
+        const target = getFilename(file, size)
+        if (skipOptimized && (await exists(target))) return
+        return image
           .resize({
             width: size,
             height: Math.round(size / aspectRatio),
@@ -46,8 +73,8 @@ export const zimg = async (options: ZImgOptions = {}) => {
             withoutEnlargement: true
           })
           .webp({ quality })
-          .toFile(file.replace(/\.[a-z]+$/i, `.${size}.webp`))
-      )
+          .toFile(target)
+      })
     )
   })
   try {
